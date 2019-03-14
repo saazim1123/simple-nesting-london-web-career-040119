74decdea26baf0dd1d1c00ade0742d77c4bf4faf
Adnan Azim <saazim1123@gmail.com>
2019-03-14 03:08:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,7 +107,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      #alans_new_info = "GUI"
      
-     programmer_hash.each
+     programmer_hash.each do |key, value|
+       
+     end
 end
 
 def adding_to_dennis
